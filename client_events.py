59c153507ea8d3db4429697f7f8eef2217ada9e1
Nick Conklin <nconkl1@outlook.com>
2017-09-13 19:10:44 -0400

    Fix bot_token

--- client_events.py
+++ client_events.py
@@ -4,3 +4,8 @@ import discord
 async def on_ready():
     print('Logged in as {}({})'.format(client.user.name, client.user.id))
     print('--------------------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+        await client.send.message(message.channel, 'Pong!')
